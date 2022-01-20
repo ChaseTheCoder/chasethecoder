1a4d93f1eed47f0937e71be412cadd90c4184bc7
Chase Sheaff <chasefsheaff@gmail.com>
2022-01-19 21:49:19 -0600

    feat: index page displaying posts

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -6,3 +6,9 @@ function portfolio_files(){
 }
 
 add_action('wp_enqueue_scripts', 'portfolio_files');
+
+function portfolio_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'portfolio_features');
--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,1 +1,11 @@
-<?php //intentionally blank ?>
+<?php get_header(); ?>
+
+<?php 
+  while(have_posts()){
+    the_post(); ?>
+    <div>
+      <h4><a href="<?php the_permalink(); ?>"><?php the_title();  ?></a></h2>
+    </div>
+  <?php } ?>
+
+<?php get_footer(); ?>
