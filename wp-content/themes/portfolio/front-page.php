4414d707e93c2ac09cda0d1667408b25490bb6a8
Chase Sheaff <chasefsheaff@gmail.com>
2022-02-13 22:08:48 -0600

    feature: add featured image to posts and pages

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
 
-<?php get_template_part('/partials/spotlight'); ?>
+<?php if(have_posts()): ?>
+  <?php while(have_posts()): the_post();?>
+    <div class="jumbotron spotlight row">
+      <div class="col-6">
+        <h1 class="display-4">Hello, I'm Chase Sheaff</h1>
+        <p class="lead">Software engineer, with strong soft skills.</p>
+      </div>
+      <div class="col-6">
+        <img class="spotlight-image"><?php the_post_thumbnail(); ?></img>
+      </div>
+    </div>
+  <?php endwhile; ?>
+<?php endif; ?>
+
 
 <?php get_footer(); ?>
--- wp-content/themes/portfolio/partials/spotlight.php
+++ wp-content/themes/portfolio/partials/spotlight.php
@@ -1,4 +1,4 @@
-<div class="jumbotron">
+<div class="jumbotron spotlight">
   <row>
     <h1 class="display-4">Hello, I'm Chase Sheaff</h1>
     <p class="lead">Software engineer, with strong soft skills.</p>
