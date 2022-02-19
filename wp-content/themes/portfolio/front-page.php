391435d123062b8ddf4833093edecf15e0e0c36d
Chase Sheaff <chasefsheaff@gmail.com>
2022-02-19 16:35:56 -0600

    style: about section and general section

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -2,17 +2,33 @@
 
 <?php if(have_posts()): ?>
   <?php while(have_posts()): the_post();?>
-    <div class="jumbotron spotlight row">
+    <div class="jumbotron spotlight row pb-5">
       <div class="col-md-6 col-12 align-self-end spotlight-left">
           <p class="front-header">Hello, I'm </br>Chase Sheaff.</p>
           <p class="front-subheader">Software engineer with strong soft skills to learn any hard skill required.</p>
         <div class="front-circle"></div>
       </div>
       <div class="col-md-6 col-12 spotlight-right">
-        <img class="spotlight-image" src="<?php the_post_thumbnail();"" ?>
+        <?php the_post_thumbnail();?>
       </div>
     </div>
   <?php endwhile; ?>
+<?php endif; ?>
+
+<?php  $fields = get_fields(); ?>
+<?php if( $fields ): ?>
+  <hr/>
+  <div class="row section about-section py-5">
+    <div class="col-12 col-md-6 col-lg-8">
+      <div class="about-content">
+        <h1><?php echo $fields['about_title']; ?></h1>
+        <p><?php echo $fields['about_content']; ?></p>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+      <image class="about-image image" src="<?php echo $fields['about_image']; ?>" />
+    </div>
+  </div>
 <?php endif; ?>
 
 
