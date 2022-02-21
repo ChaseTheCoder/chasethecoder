b4f562959db577fd8f8fa87bb125c369fa37cbc5
Chase Sheaff <chasefsheaff@gmail.com>
2022-02-20 22:58:25 -0600

    style: footer responsive for mobile

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,11 +1,11 @@
   <hr>
   <div class="row footer">
-    <div class="align-left col-6">
+    <div class="col-6 footer-column">
       <p>Chase Sheaff <script>document.write(new Date().getFullYear())</script></p>
     </div>
-    <div class="col-6 text-end">
-      <a class="footer-right-text" href="https://github.com/chaseTheCoder" target="_blank">GitHub <i class="fa-brands fa-github"></i></a> 
-      <a class="footer-right-text" href="https://www.linkedin.com/in/chasethecoder/" target="_blank">LinkedIn <i class="fa-brands fa-linkedin"></i></a>
+    <div class="col-6 footer-column text-end">
+      <a class="footer-right-text" href="https://github.com/chaseTheCoder" target="_blank">GitHub<i class="footer-icon fa-brands fa-github"></i></a> 
+      <a class="footer-right-text" href="https://www.linkedin.com/in/chasethecoder/" target="_blank">LinkedIn<i class="footer-icon fa-brands fa-linkedin"></i></a>
     </div>
     <?php wp_footer(); ?>
   </div>
