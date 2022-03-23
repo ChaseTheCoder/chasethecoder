41173139e1dfa92340fbca28b8bd0ba5729d691c
Chase Sheaff <chasefsheaff@gmail.com>
2022-03-22 21:15:36 -0500

    style: nav bar width of screen

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,14 +1,18 @@
-  <hr>
-  <div class="row footer">
-    <div class="col-6 footer-column">
-      <p>Chase Sheaff <script>document.write(new Date().getFullYear())</script></p>
-    </div>
-    <div class="col-6 footer-column text-end">
-      <a class="footer-right-text" href="https://github.com/chaseTheCoder" target="_blank">GitHub<i class="footer-icon fa-brands fa-github"></i></a> 
-      <a class="footer-right-text" href="https://www.linkedin.com/in/chasethecoder/" target="_blank">LinkedIn<i class="footer-icon fa-brands fa-linkedin"></i></a>
-    </div>
-    <?php wp_footer(); ?>
+  
   </div>
+  <div class="footer-section">
+    <hr>
+    <div class="row footer">
+      <div class="col-6 footer-column">
+        <p>Chase Sheaff <script>document.write(new Date().getFullYear())</script></p>
+      </div>
+      <div class="col-6 footer-column text-end">
+        <a class="footer-right-text" href="https://github.com/chaseTheCoder" target="_blank">GitHub<i class="footer-icon fa-brands fa-github"></i></a> 
+        <a class="footer-right-text" href="https://www.linkedin.com/in/chasethecoder/" target="_blank">LinkedIn<i class="footer-icon fa-brands fa-linkedin"></i></a>
+      </div>
+      <?php wp_footer(); ?>
+  </div>
+</div>
 </body>
 </html>
 
